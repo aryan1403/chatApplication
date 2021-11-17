93d04213096c6b420f5dcb9f4495fd80ddb209a2
Satyendra7 <93898698+Satyendra7@users.noreply.github.com>
2021-11-17 10:04:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '92781cba-70c4-4d43-826a-3b3de3b383f4';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      // userName={localStorage.getItem('username')}
+      // userSecret={localStorage.getItem('password')}
+      userName="satyendra"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
